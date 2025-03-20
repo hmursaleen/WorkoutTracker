ef489442a2348dd43f13a01087e717b9bcd3e9a9
habibulmursaleen@gmail.com <habibulmursaleen@gmail.com>
2025-03-20 17:11:23 +0600

    added user registration endpoint

---
+++ users/serializers.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+    password2 = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'password2')
+        extra_kwargs = {
+            'email': {'required': True}
+        }
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
+
+    def create(self, validated_data): #create a user
+        validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
+        return user
